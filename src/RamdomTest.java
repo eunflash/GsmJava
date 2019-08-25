da5b0f1a2668906a7898cf4b4d4f0a6fc3bc6f56
eunflash <eunflash@naver.com>
2019-08-25 22:10:10 +0900

    Math.random

---
+++ src/RamdomTest.java
@@ -0,0 +1,17 @@
+
+public class RamdomTest {
+
+	public static void main(String[] args) {
+		
+		double r = 0;
+		
+		//1부터 10까지 난수 구히기
+		for(int i=0 ; i<10; i++) {
+			r = Math.random()*10+1;
+			int rand_int = (int)Math.round(r);
+			System.out.println(rand_int);
+		}
+		
+	}
+
+}
